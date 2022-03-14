95493fba162bf3cc9752a8883bde6d960cd6aaf9
hasan-ullah <mdhasanullah124@gmail.com>
2022-03-14 06:59:46 +0600

    http method testing

--- express/app.js
+++ express/app.js
@@ -2,10 +2,24 @@ const express = require('express');
 const app = express();
 
 app.get("/",(req,res)=>{
-    res.send("i'm get requiest")
+    res.send("i'm get requiest");
+    res.end();
 });
 app.get("/about",(req,res)=>{
-    res.send("i'm get requiest at about")
+    res.send("i'm get requiest at about");
+    res.end();
+});
+app.post("/",(req,res)=>{
+    res.send("i'm post requiest ");
+    res.end();
+});
+app.put("/",(req,res)=>{
+    res.send("i'm post requiest ");
+    res.end();
+});
+app.delete("/",(req,res)=>{
+    res.send("i'm post requiest ")
+    res.end();
 });
 
 module.exports = app;
