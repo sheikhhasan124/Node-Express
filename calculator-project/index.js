ed8b7178a0ab803517cba3357a9a9a5218ae9cbf
hasan-ullah <mdhasanullah124@gmail.com>
2022-03-15 13:59:35 +0600

    pages add

--- calculator-project/index.js
+++ calculator-project/index.js
@@ -3,6 +3,19 @@ const app = express();
 const PORT = 3000;
 
 
+
+app.get("/",(req,res)=>{
+    res.sendFile(__dirname+"/index.html")
+})
+app.get("/circle",(req,res)=>{
+    res.sendFile(__dirname+"/circle.html")
+})
+app.get("/triangle",(req,res)=>{
+    res.sendFile(__dirname+"/triangle.html")
+})
+
+
+
 app.listen(PORT,()=>{
      console.log(`server is runnig at:http://localhost:${PORT}`)
 })
